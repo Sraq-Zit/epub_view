0a32e6628a21e28340b1029e9adc82e0af14e9d3
SraqZit <kingofmestry@gmail.com>
2022-05-24 22:31:36 +0100

    test

--- lib/src/ui/epub_view.dart
+++ lib/src/ui/epub_view.dart
@@ -354,7 +354,7 @@ class _EpubViewState extends State<EpubView> {
               return url.startsWith(RegExp(r'https?://'))
                   ? SvgPicture.network(
                       url,
-                      fit: BoxFit.fitWidth,
+                      fit: BoxFit.cover,
                     )
                   : Image(
                       image: MemoryImage(
